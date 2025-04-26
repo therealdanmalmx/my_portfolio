7d72474337cfab2024cdf8d5faa039b89ec8fe08
therealdanmalmx <danmalmx@gmail.com>
2025-04-26 22:01:04 +0200

    Add links to social icons

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,16 +6,16 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-[100px] w-full px-12">
             <div><a href="#home">Logo</a></div>
             <div className="hidden md:block">
-                <ul className="flex items-center my-2">
+                <ul className="flex items-center space-x-6">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#work">Work</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
-            <div className="hidden md:flex items-start md:items-center space-x-2">
-                <div><Linkedin /></div>
-                <div><Github /></div>
+            <div className="hidden md:flex items-start md:items-center space-x-4">
+                <a href="https://www.linkedin.com/in/daniel-malmgren-de-oliveira"><Linkedin /></a>
+                <a href="https://github.com/therealdanmalmx"><Github /></a>
             </div>
         </div>
     );
