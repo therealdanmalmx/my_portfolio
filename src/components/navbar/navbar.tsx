cb5f84c96ac1c6ee146426f88c281693c0aa1e5a
therealdanmalmx <danmalmx@gmail.com>
2025-04-26 21:55:17 +0200

    Update navbar with social icons

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,11 @@
-
+import Linkedin from "../../../public/icons/linkedin.tsx";
+import Github from "../../../public/icons/github.tsx";
 
 const Navbar = () => {
     return (
         <div className="flex justify-between items-center h-[100px] w-full px-12">
             <div><a href="#home">Logo</a></div>
-            <div>
+            <div className="hidden md:block">
                 <ul className="flex items-center my-2">
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
@@ -12,13 +13,10 @@ const Navbar = () => {
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
-            <div className="flex items-center space-x-12">
-                <div>LinkedIn</div>
-                <div>GitHub</div>
-
+            <div className="hidden md:flex items-start md:items-center space-x-2">
+                <div><Linkedin /></div>
+                <div><Github /></div>
             </div>
-
-            
         </div>
     );
 };
